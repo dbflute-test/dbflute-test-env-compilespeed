6f8742c804f3b6fed43c172fc6450c70a21348d3
jflute <dbflute@gmail.com>
2016-09-02 00:09:30 +0900

    add /usr/local/bin/ for new MacOS

--- maven-compile.sh
+++ maven-compile.sh
@@ -4,4 +4,5 @@ if [ `uname` = "Darwin" ]; then
   export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
 fi
 
-mvn -e clean compile
+# path does not have /usr/local/bin from Java for unexplained reason
+/usr/local/bin/mvn -e clean compile
